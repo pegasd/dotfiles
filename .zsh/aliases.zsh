46ed47011286a46a09fb5e9ec245282b2d9db6e5
Eugene Piven <thepegasd@gmail.com>
2019-06-27 16:14:17 +0300

    Add brewup alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -47,3 +47,4 @@ alias anplay='time ansible-playbook --diff'
 # macOS
 alias pbud='/usr/libexec/PlistBuddy'
 alias xee='open -a xee³'
+alias brewup='brew update; brew upgrade $(brew outdated); brew cask upgrade $(brew cask outdated --greedy)'
